b2f71b7e9e8b1d897eb582c5e9a3c7925a9f0b6d
Rui Fernandes <rpaf.fernandes@gmail.com>
2025-03-26 16:29:49 +0000

    Add product microservice with CRUD operations and DynamoDB integration

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  globalIgnores(["cdk.out/**", "dist/**", "test/**", "jest.config.js"]),
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    rules: {
+      "max-len": ["error", { code: 80, ignoreUrls: true }],
+    },
+  },
+  tseslint.configs.recommended,
+]);
--- lib/aws-microservices-stack.ts
+++ lib/aws-microservices-stack.ts
@@ -1,16 +1,70 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
+import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import {
+  NodejsFunction,
+  NodejsFunctionProps,
+} from "aws-cdk-lib/aws-lambda-nodejs";
+import { Construct } from "constructs";
+import * as path from "path";
 
 export class AwsMicroservicesStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const productTable = new Table(this, "ProductTable", {
+      tableName: "product",
+      partitionKey: {
+        name: "id",
+        type: AttributeType.STRING,
+      },
+      billingMode: BillingMode.PROVISIONED,
+      readCapacity: 5,
+      writeCapacity: 10,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsMicroservicesQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const nodeJsFunctionProps: NodejsFunctionProps = {
+      bundling: {
+        externalModules: ["aws-sdk"],
+      },
+      environment: {
+        PRIMARY_KEY: "id",
+        DYNAMODB_TABLE_NAME: productTable.tableName,
+      },
+      runtime: Runtime.NODEJS_22_X,
+    };
+
+    const productFunction = new NodejsFunction(this, "productLambdaFunction", {
+      entry: path.join(__dirname, "/../src/product/index.js"),
+      ...nodeJsFunctionProps,
+    });
+
+    productTable.grantReadWriteData(productFunction);
+
+    // product
+    // GET /product
+    // POST /product
+
+    // product/{id}
+    // GET /product/{id}
+    // PUT /product/{id}
+    // DELETE /product/{id}
+
+    const api = new LambdaRestApi(this, "productApiGateway", {
+      restApiName: "Product Service",
+      handler: productFunction,
+      proxy: false,
+    });
+
+    const productsResource = api.root.addResource("product");
+    productsResource.addMethod("GET"); // GET /product
+    productsResource.addMethod("POST"); // POST /product
+
+    const productResource = productsResource.addResource("{id}");
+    productResource.addMethod("GET"); // GET /product/{id}
+    productResource.addMethod("PUT"); // PUT /product/{id}
+    productResource.addMethod("DELETE"); // DELETE /product/{id}
   }
 }
